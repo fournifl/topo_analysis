154aa8df89d5013e8a387e32b828b65946685287
Florent <florent.fournier@wavesnsee.com>
2026-05-13 19:56:02 +0200

    call functions "read_wcams_topo" and "plot_wcams_topo"

--- src/topo_an/core/topo_analysis.py
+++ src/topo_an/core/topo_analysis.py
@@ -1,9 +1,15 @@
+from topo_an.core.wcams_topo import read_wcams_topo, plot_wcams_topos
 
-
-def run(
-wavecams_topos,
-ponctual_topos,
+def run(wavecams_topos,
+        ponctual_topos,
         output_dir
 ):
-    print('')
+
+    # read wavecams topographies
+    wc_topos, bounds = read_wcams_topo(wavecams_topos.dir)
+
+    # plot wavecams topographies
+    plot_wcams_topos(wc_topos, bounds, wavecams_topos.epsg)
+
+
     return
