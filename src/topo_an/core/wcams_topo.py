6e3bead1140e86e5d2f54d35cdf9bc9acb336c04
Florent <florent.fournier@wavesnsee.com>
2026-05-18 17:13:24 +0200

    use linear segmented colormap

--- src/topo_an/core/wcams_topo.py
+++ src/topo_an/core/wcams_topo.py
@@ -1,10 +1,12 @@
 import rasterio
 import numpy as np
-from bokeh.models import WMTSTileSource, LinearColorMapper, Slider, CustomJS
-from bokeh.plotting import figure, show, save, output_file
+from bokeh.models import LinearColorMapper, Slider, CustomJS
+from bokeh.plotting import figure, save, output_file
 from bokeh.layouts import column
-import colorcet as cc
-from topo_an.core.geo_utils import XY_1_to_XY_2
+from matplotlib.colors import LinearSegmentedColormap
+from topo_an.core.geo_utils import XY_1_to_XY_2, osm_tile
+from matplotlib.colors import to_hex
+
 
 def read_wcams_topo(dir_wcams_topo):
 
@@ -21,13 +23,24 @@ def read_wcams_topo(dir_wcams_topo):
             topo = np.ma.array(data, mask=data==-9999.)# Read first band
             # meta = src.meta
             topos.append(topo)
-            if i ==0:
+            if i == 0:
                 bounds = src.bounds
             dates.append(f.stem.split('_')[-1])
+
     return topos, dates, bounds
 
+def convert_mpl_colormap_to_hex(cmap, n_colors):
 
-def plot_wcams_topos(topos, dates, bounds, epsg, tile_choice = 'Esri'):
+    # Generate colors from the colormap (e.g., 256 colors)
+    colors_rgb = cmap(np.linspace(0, 1, n_colors))
+
+    # Convert RGB values (0-1 range) to hex strings
+    palette = [to_hex(rgb) for rgb in colors_rgb]
+
+    return palette
+
+
+def plot_wcams_topos(topos, dates, bounds, epsg, output_dir, tile_choice = 'Esri'):
 
     x, y = XY_1_to_XY_2(np.array([bounds.left, bounds.right]),
                               np.array([bounds.bottom, bounds.top]),
@@ -41,23 +54,28 @@ def plot_wcams_topos(topos, dates, bounds, epsg, tile_choice = 'Esri'):
     # Convert all topo masked arrays to NaN arrays for Bokeh
     z = [np.flipud(np.where(topo.mask, np.nan, topo.data)) for topo in topos]
 
+    # customized colormap. Edit this gradient at https://eltos.github.io/gradient/#0C0A69-2B4CD9-00E55A-FBFF03-9E6800-371B00
+    cmap = LinearSegmentedColormap.from_list('my gradient', (
+        # Edit this gradient at https://eltos.github.io/gradient/#0C0A69-2B4CD9-00E55A-FBFF03-F2B513-9E6800-371B00
+        (0.000, (0.047, 0.039, 0.412)),
+        (0.167, (0.169, 0.298, 0.851)),
+        (0.333, (0.000, 0.898, 0.353)),
+        (0.500, (0.984, 1.000, 0.012)),
+        (0.667, (0.949, 0.710, 0.075)),
+        (0.833, (0.620, 0.408, 0.000)),
+        (1.000, (0.216, 0.106, 0.000))))
+    palette = convert_mpl_colormap_to_hex(cmap, 256)
+
     # Setup color mapper
-    color_mapper = LinearColorMapper(palette=cc.rainbow, low=-3, high=4)
+    color_mapper = LinearColorMapper(palette=palette, low=-3, high=4)
     color_mapper.nan_color = (0, 0, 0, 0)
 
     # Create figure
     p = figure(title="Intertidal topography", width=1536, height=864, x_axis_type="mercator", y_axis_type="mercator",
                match_aspect=True)
 
-    # ---- Add OSM tiles ----
-    if tile_choice == 'carto_light':
-        tile = WMTSTileSource(
-            url='https://cartodb-basemaps-a.global.ssl.fastly.net/light_all/{Z}/{X}/{Y}.png',
-            attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions">CARTO</a>'
-        )
-    elif tile_choice == "Esri":
-        tile = "Esri World Imagery"
-    p.add_tile(tile)
+    # Add OSM tiles
+    p.add_tile(osm_tile(tile_choice))
 
     # Hide grid lines
     p.grid.visible = False
@@ -81,7 +99,7 @@ def plot_wcams_topos(topos, dates, bounds, epsg, tile_choice = 'Esri'):
 
     slider.js_on_change('value', callback)
 
-    output_file('test.html')
+    output_file(output_dir.joinpath('wcams_topos.html'))
     layout = column(slider, p)
     save(layout)
 
