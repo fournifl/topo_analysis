da1c90e3c2afddc87d6c308b9ac39e8169158067
Florent <florent.fournier@wavesnsee.com>
2026-06-04 15:34:53 +0200

    keep rio objects when reading wavecams topographies

--- src/topo_an/core/wcams_topo.py
+++ src/topo_an/core/wcams_topo.py
@@ -1,117 +1,27 @@
 import rasterio
-import numpy as np
-from bokeh.models import LinearColorMapper, Slider, CustomJS
-from bokeh.plotting import figure, save, output_file
-from bokeh.layouts import column
-from matplotlib.colors import LinearSegmentedColormap
-from topo_an.core.geo_utils import XY_1_to_XY_2, osm_tile
-from matplotlib.colors import to_hex
+from rasterio.crs import CRS
 
-
-def read_wcams_topo(dir_wcams_topo):
+def read_wcams_topo(dir_wcams_topo, epsg):
 
     # list of wavecams ascii topo files
     ls = sorted(dir_wcams_topo.glob('*.asc'))
 
-    # output list of topographies
-    topos = []
+    # output list of rio topography objects
+    rio_topos = []
     dates = []
 
     for i, f in enumerate(ls):
-        with rasterio.open(f) as src:
-            data = src.read(1)
-            topo = np.ma.array(data, mask=data==-9999.)# Read first band
-            # meta = src.meta
-            topos.append(topo)
-            if i == 0:
-                bounds = src.bounds
-            dates.append(f.stem.split('_')[-1])
 
-            # grid coordinates
-            cols, rows = np.meshgrid(np.arange(src.width), np.arange(src.height))
-            grid_x, grid_y = rasterio.transform.xy(src.transform, rows, cols)
+        # create rio object
+        src = rasterio.open(f, 'r+')
 
-            # grid_resolution
-            x_res = src.transform[0]
-            y_res = - src.transform[4]
+        # set crs
+        src.crs = CRS.from_epsg(epsg)
 
-            # surface of a pixel
-            sp = x_res * y_res
+        # append rio object to list
+        rio_topos.append(src)
 
-    return topos, dates, bounds, sp
+        # append date to dates
+        dates.append(f.stem.split('_')[-1])
 
-def convert_mpl_colormap_to_hex(cmap, n_colors):
-
-    # Generate colors from the colormap (e.g., 256 colors)
-    colors_rgb = cmap(np.linspace(0, 1, n_colors))
-
-    # Convert RGB values (0-1 range) to hex strings
-    palette = [to_hex(rgb) for rgb in colors_rgb]
-
-    return palette
-
-
-def plot_wcams_topos(topos, dates, bounds, epsg, output_dir, tile_choice = 'Esri'):
-
-    x, y = XY_1_to_XY_2(np.array([bounds.left, bounds.right]),
-                              np.array([bounds.bottom, bounds.top]),
-                              epsg,
-                              '3857')
-    x_min = np.min(x)
-    x_max = np.max(x)
-    y_min = np.min(y)
-    y_max = np.max(y)
-
-    # Convert all topo masked arrays to NaN arrays for Bokeh
-    z = [np.flipud(np.where(topo.mask, np.nan, topo.data)) for topo in topos]
-
-    # customized colormap, # Edit this gradient at
-    # https://eltos.github.io/gradient/#0C0A69-2A5FD9-00E55A-FBFF03-F2B513-8B6316-371B00
-    cmap = LinearSegmentedColormap.from_list('my gradient', (
-    (0.000, (0.047, 0.039, 0.412)),
-    (0.167, (0.165, 0.373, 0.851)),
-    (0.333, (0.000, 0.898, 0.353)),
-    (0.500, (0.984, 1.000, 0.012)),
-    (0.667, (0.949, 0.710, 0.075)),
-    (0.833, (0.545, 0.388, 0.086)),
-    (1.000, (0.216, 0.106, 0.000))))
-    palette = convert_mpl_colormap_to_hex(cmap, 256)
-
-    # Setup color mapper
-    color_mapper = LinearColorMapper(palette=palette, low=-3, high=4)
-    color_mapper.nan_color = (0, 0, 0, 0)
-
-    # Create figure
-    p = figure(title="Intertidal topography", width=1536, height=864, x_axis_type="mercator", y_axis_type="mercator",
-               match_aspect=True)
-
-    # Add OSM tiles
-    p.add_tile(osm_tile(tile_choice))
-
-    # Hide grid lines
-    p.grid.visible = False
-
-    # plot topo
-    img = p.image(image=[z[0]], x=x_min, y=y_min, dw=(x_max - x_min), dh=(y_max - y_min), color_mapper=color_mapper)
-
-    # Create slider with CustomJS callback
-    slider = Slider(start=0, end=len(z) - 1, step=1, value=0, title="INTERTIDAL TOPOGRAPHY", format=" ", width=1200)
-
-    callback = CustomJS(args=dict(img=img,
-                                  arrays=z,
-                                  slider=slider,
-                                  p=p,
-                                  dates=dates), code="""
-        const idx = slider.value;
-        img.data_source.data['image'][0] = arrays[idx];
-        img.data_source.change.emit();
-        p.title.text = `${dates[idx]}`;
-    """)
-
-    slider.js_on_change('value', callback)
-
-    output_file(output_dir.joinpath('wcams_topos.html'))
-    layout = column(slider, p)
-    save(layout)
-
-    return
+    return rio_topos, dates
