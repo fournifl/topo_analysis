1c5b16ff45ef97662b6207e384462f054f7bbda6
Florent <florent.fournier@wavesnsee.com>
2026-06-08 11:32:44 +0200

    rename read to open

--- src/topo_an/core/topo_analysis.py
+++ src/topo_an/core/topo_analysis.py
@@ -1,4 +1,4 @@
-from topo_an.core.topo import read_wcams_topo, read_sporadic_topos, apply_roi_mask_to_sporadic_topos
+from topo_an.core.topo import open_wcams_topo, open_sporadic_topos, apply_roi_mask_to_sporadic_topos
 from topo_an.core.plot import plot_topos
 from topo_an.core import stats
 
@@ -7,8 +7,8 @@ def run(wavecams_topos,
         output_dir
 ):
 
-    # read wavecams topographies
-    wc_rio_topos, wc_dates = read_wcams_topo(wavecams_topos.dir, wavecams_topos.epsg)
+    # open wavecams topographies
+    wc_rio_topos, wc_dates = open_wcams_topo(wavecams_topos.dir, wavecams_topos.epsg)
 
     # plot wavecams topographies
     plot_topos(wc_rio_topos, wc_dates, output_dir, name_out='wcams_topos', low=-3, high=4, name='WAVECAMS')
@@ -25,8 +25,8 @@ def run(wavecams_topos,
     for ds in wc_rio_topos:
         ds.close()
 
-    # read sporadic topographies
-    sp_rio_topos = read_sporadic_topos(sporadic_topos.files, sporadic_topos.epsg)
+    # open sporadic topographies
+    sp_rio_topos = open_sporadic_topos(sporadic_topos.files, sporadic_topos.epsg)
 
     # apply roi mask to sporadic topographies
     sp_rio_topos = apply_roi_mask_to_sporadic_topos(sp_rio_topos, sporadic_topos.roi, output_dir)
