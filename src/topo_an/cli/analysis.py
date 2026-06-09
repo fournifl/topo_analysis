47747fb45ba89c24aa71054b700cf9ef5d5296bf
Florent <florent.fournier@wavesnsee.com>
2026-06-09 14:10:05 +0200

    launch sequentially run_wcams, run_sporadic, and run_all

--- src/topo_an/cli/analysis.py
+++ src/topo_an/cli/analysis.py
@@ -2,15 +2,15 @@ from topo_an.core import topo_analysis
 
 def main(conf):
 
-    # topo_analysis.run_wcams(
-    #     conf.wavecams_topos,
-    #     conf.output_dir
-    # )
-    #
-    # topo_analysis.run_spor(
-    #     conf.sporadic_topos,
-    #     conf.output_dir
-    # )
+    topo_analysis.run_wcams(
+        conf.wavecams_topos,
+        conf.output_dir
+    )
+
+    topo_analysis.run_spor(
+        conf.sporadic_topos,
+        conf.output_dir
+    )
 
     topo_analysis.run_all(
         conf.wavecams_topos,
